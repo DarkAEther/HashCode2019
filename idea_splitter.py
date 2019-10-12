e22dcd27a10155ddf8f72d41db5b7081570a04d9
DarkAEther <30438425+DarkAEther@users.noreply.github.com>
2019-10-12 20:12:41 +0530

    Tested xlsx code

--- idea_splitter.py
+++ idea_splitter.py
@@ -1,26 +1,45 @@
-import pandas as pd
 import os
 import re
+import openpyxl
+from collections import OrderedDict
+TITLE=0
+DESCRIPTION=1
+ATTACHMENT=2
+TEAM_NAME=3
+THEME=4 #unused - only 1 theme
+STATUS=5
+LEADER_NAME=6
+LEADER_EMAIL=7
 
-def get_domain(description):
-    '''Returns domain of the description'''
-    description = description.lower()
-    print(description)
-    domains = re.findall("^domain[.: -]*[a-z]*",description)
-    print(domains)
-    if (not domains):
-        return 0
-    return domains[0]
+raw_projects =[]
+clean_projects = OrderedDict()
+raw_projects = openpyxl.load_workbook("hashcode-idea-submissions.xlsx")
+sheet = raw_projects.active
 
-def write_record_to_file(domain, record):
-    '''Writes the given record to the appropriate file'''
-    #TODO
+final_xlsx = openpyxl.Workbook()
+final_sheet = final_xlsx.active
+row_count =0
+headers =''
+for row in sheet.iter_rows(min_row=1, min_col=1, max_col=8): #iterate over all rows
+    #first remove disqualified idea and get the latest idea
+    dlist= []
+    
+    for cell in row:
+        dlist.append(cell.value)
+    if (row_count== 0): #skip header processing
+        row_count+=1
+        headers = dlist
+        continue
+    print("Processing idea number ",row_count)
+    if (dlist[STATUS] != "Disqualified"): #not disqualified
+        clean_projects[dlist[TEAM_NAME]] = (tuple(dlist),row_count) #overwrites data, hence only latest idea remains
+    row_count+=1
 
-data = pd.read_csv("hashcode-idea-submissions.csv")
-data = pd.DataFrame(data)
-
-for ind,row in data.iterrows(): #iterate over all records
-    domain = get_domain(row['Description'])
-    if (not pd.isna(row['Attachment'])):
-        print("wgetting attach", row["Attachment"])
-        #os.system("wget -O "+"./domains/"+domain+"/"+row['Idea title']+".pdf "+row['Attachment'])
+final_sheet.append(headers)
+final_clean_projects = sorted(clean_projects.values(),key = lambda x: x[1])
+for item in final_clean_projects:
+    final_sheet.append(item[0])
+    if (item[0][ATTACHMENT]!=None):
+        print("WILL GET", item[0][ATTACHMENT])
+        #os.system("wget -O "+"./attachments/"+clean_projects[key][TITLE]+".pdf "+clean_projects[key][ATTACHMENT])
+final_xlsx.save("final_ideas.xlsx")
